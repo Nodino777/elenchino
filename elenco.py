568cdfcb4c1d0d071d564c1f2772d816143021a0
Nodino777 <angelo.arrigoni@cia.it>
2025-03-07 15:47:21 +0100

    Update elenco.py
    Cambiato titolo

--- elenco.py
+++ elenco.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Set page configuration as the first Streamlit command
 st.set_page_config(
-    page_title="CSV Data Viewer & Analyzer",
+    page_title="Elenco stato PCG delle aziende",
     page_icon="📊",
     layout="wide"
 )
